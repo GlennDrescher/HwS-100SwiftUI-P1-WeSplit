bb86a58acf2704368f20b592b2db8167f8fdb33b
Glenn Drescher <GlennD@me.com>
2021-11-17 15:20:56 +0100

    Initial Commit

---
+++ HwS-100SwiftUI-P1-WeSplit/HwS_100SwiftUI_P1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HwS_100SwiftUI_P1_WeSplitApp.swift
+//  HwS-100SwiftUI-P1-WeSplit
+//
+//  Created by Glenn Drescher on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct HwS_100SwiftUI_P1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
